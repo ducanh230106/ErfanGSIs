c1a85bf481b6e719e732edd8b9c112489e8977ec
ducanh230106 <75192488+ducanh230106@users.noreply.github.com>
2021-03-13 19:54:31 +0700

    Update debloat.sh

--- roms/10/MIUI/debloat.sh
+++ roms/10/MIUI/debloat.sh
@@ -3,13 +3,8 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-rm -rf $1/app/cit
-rm -rf $1/app/MiuiCompass
-rm -rf $1/app/MiuiScreenRecorder
 rm -rf $1/app/MiuiVideoGlobal
-rm -rf $1/app/Notes
 rm -rf $1/app/PaymentService
-rm -rf $1/app/Calculator
 rm -rf $1/app/AnalyticsCore
 rm -rf $1/app/BasicDreams
 rm -rf $1/app/BookmarkProvider
@@ -25,29 +20,48 @@ rm -rf $1/app/MiuiDaemon
 rm -rf $1/app/BuiltInPrintService
 rm -rf $1/app/PrintSpooler
 rm -rf $1/app/SoterService
-rm -rf $1/priv-app/Backup
 rm -rf $1/priv-app/Browser
-rm -rf $1/priv-app/Calendar
-rm -rf $1/priv-app/CleanMaster
-rm -rf $1/priv-app/MiRecycle
-rm -rf $1/priv-app/MiuiScanner
 rm -rf $1/priv-app/MiWebView
 rm -rf $1/priv-app/Updater
 rm -rf $1/priv-app/Velvet
-rm -rf $1/priv-app/Weather
-rm -rf $1/priv-app/WeatherProvider
 rm -rf $1/priv-app/PersonalAssistant
 rm -rf $1/priv-app/BackupAndRestore
+rm -rf $1/priv-app/MiService
+rm -rf $1/app/MSA-Global
+rm -rf $1/priv-app/PersonalAssistant
+rm -rf $1/app/BugReport
+rm -rf $1/app/Drive
+rm -rf $1/app/Duo
+rm -rf $1/app/Gmail2
+rm -rf $1/app/Maps
+rm -rf $1/app/Music2
+rm -rf $1/app/Photos
+rm -rf $1/app/Videos
+rm -rf $1/app/YouDaoEngine
+rm -rf $1/app/YouTube
+rm -rf $1/app/facebook-appmanager
+rm -rf $1/app/talkback
+rm -rf $1/priv-app/MiDrop
+rm -rf $1/priv-app/PersonalAssistantGlobal
+rm -rf $1/priv-app/YellowPage
+rm -rf $1/priv-app/facebook-installer
+rm -rf $1/priv-app/facebook-services
+rm -rf $1/product/app/Gmail2
+rm -rf $1/product/app/GoogleTTS
+rm -rf $1/product/app/Maps
+rm -rf $1/product/app/YouTube
+rm -rf $1/app/FileExplorer_old
+rm -rf $1/app/Netflix_activation
+rm -rf $1/data-app/Email
+rm -rf $1/app/MiuiVideoPlayer
+rm -rf $1/priv-app/MiuiMusicGlobal
 
 rm -rf $1/priv-app/SoundRecorder
-rm -rf $1/data-app/CleanMaster
 rm -rf $1/data-app/GameCenter
 rm -rf $1/data-app/Huanji
 rm -rf $1/data-app/MiDrive
 rm -rf $1/data-app/MiFinance 
-rm -rf $1/data-app/MiuiCompass
-rm -rf $1/data-app/MiuiScanner
-rm -rf $1/data-app/Notes
 rm -rf $1/data-app/SmartTravel
-rm -rf $1/data-app/Weather
+rm -rf $1/data-app/MiGalleryLockscreen
+rm -rf $1/data-app/Health
 
